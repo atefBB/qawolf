a67b2f09b52d5a02b8c43b9bc867b914c59ad0d8
Atef Ben Ali <atefBB@users.noreply.github.com>
2024-07-17 13:18:12 +0200

    refactor(utilsPick): use `named export`

--- src/utils/pick.ts
+++ src/utils/pick.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // Since we only need this for now, we can avoid downloading lodash
-const pick = (
+export const pick = (
   obj: Record<any, any>,
   props: readonly string[]
 ): Record<any, any> => {
@@ -12,5 +12,3 @@ const pick = (
   }
   return newObj;
 };
-
-export default pick;
